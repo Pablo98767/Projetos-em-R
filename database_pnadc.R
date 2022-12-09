f2281e06a5e6b2f1727c46018cac6e0955a84dfa
Pablo Felix <89041026+Pablo98767@users.noreply.github.com>
2022-12-09 20:18:29 -0300

    Add files via upload
    Neste repositório utilizo a linguagem R para extrair as bases de dados da PNAD Contínua do IBGE e converter o dataframe da base em um csv e salvar em um diretório. (Script Real utilizado na startup Observatório Fieg)

---
+++ database_pnadc.R
@@ -0,0 +1,187 @@
+#importando biblioteca da pnad contínua
+library(PNADcIBGE)
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2013
+
+#extraindos todos os dados da pnadc de 2013
+dadosPNADc_2013 <- get_pnadc(year=2013, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2013<-dadosPNADc_2013$variable
+
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2013, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2013.csv", row.names = F)
+
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2014
+
+#extraindos todos os dados da pnadc de 2014
+dadosPNADc_2014 <- get_pnadc(year=2014, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2014<-dadosPNADc_2014$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2014, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2014.csv", row.names = F)
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2015
+
+#extraindos todos os dados da pnadc de 2015
+dadosPNADc_2015 <- get_pnadc(year=2015, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2015<-dadosPNADc_2015$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2015, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2015.csv", row.names = F)
+
+
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2016
+
+#extraindos todos os dados da pnadc de 2016
+dadosPNADc_2016 <- get_pnadc(year=2016, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2016<-dadosPNADc_2016$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2016, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2016.csv", row.names = F)
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2017
+
+#extraindos todos os dados da pnadc de 2017
+dadosPNADc_2017 <- get_pnadc(year=2017, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2017<-dadosPNADc_2017$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2017, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2017.csv", row.names = F)
+
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2018
+
+#extraindos todos os dados da pnadc de 2018
+dadosPNADc_2018 <- get_pnadc(year=2018, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2018<-dadosPNADc_2018$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2018, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2018.csv", row.names = F)
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2019
+
+#extraindos todos os dados da pnadc de 2019
+dadosPNADc_2019 <- get_pnadc(year=2019, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2019<-dadosPNADc_2019$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2019, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2019.csv", row.names = F)
+
+
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2020
+
+#extraindos todos os dados da pnadc de 2020
+dadosPNADc_2020 <- get_pnadc(year=2020, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2020<-dadosPNADc_2020$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2020, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2020.csv", row.names = F)
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2021
+
+#extraindos todos os dados da pnadc de 2021
+dadosPNADc_2021 <- get_pnadc(year=2021, quarter=4)
+
+#transformando em um dataframe.
+dadosPNADc_2021<-dadosPNADc_2021$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2021, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2021.csv", row.names = F)
+
+
+
+
+
+
+
+
+# EXTRAINDO DADOS DA PNAD CONTÍNUA 2022
+
+#extraindos todos os dados da pnadc de 2022
+dadosPNADc_2022 <- get_pnadc(year=2022, quarter=3)
+
+#transformando em um dataframe.
+dadosPNADc_2022<-dadosPNADc_2022$variable
+
+
+# convertendo para csv e salvando em um diretório
+write.csv2(dadosPNADc_2022, "G:/IEL/OBSERVATORIO/BASES DE DADOS OBSERVATÓRIO/PNAD/Banco de Dados/base de dados tratada/base de dados tratada/pnadc_2022.csv", row.names = F)
+
+
+
+
+
+
+
+
+
+
